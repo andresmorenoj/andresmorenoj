dd85904e9bb1a17cd53b996b305694d3b5e5d922
= <=>
2020-12-09 16:24:55 -0500

    [ADD] structure added for components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
 
-const About = () => <h1>¡Hola, Mundo!</h1>;
+const About = () => (
+  <div className="About">
+    <div className="About-container">
+      <div className="About-avatar">
+        <figure>
+          <img src="/" alt="" />
+        </figure>
+      </div>
+      <div className="About-name">
+        <h2>Andrés Moreno</h2>
+      </div>
+      <div className="About-profession">
+        <p>Web UI Developer en Globant | React.js</p>
+      </div>
+      <div className="About-desc">
+        <p>Soy desarrollador web y me encantan el anime</p>
+      </div>
+      <div className="About-location">
+        <p>Medellín - Colombia</p>
+      </div>
+      <div className="About-social">Social</div>
+    </div>
+  </div>
+);
 
 export default About;
---
+++ src/components/Experience.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Experience = () => (
+  <div className="Experience">
+    <div className="Experience-container">
+      <div className="Experience-item">
+        <h3>Community Manager</h3>
+        <p>Líneas y Diseños</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default Experience;
