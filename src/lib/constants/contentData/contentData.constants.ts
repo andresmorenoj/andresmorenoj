0f75d4cba5b01d4f41357bbfc2e382c3fb4e039e
Andres Moreno <andresmorenojf@gmail.com>
2024-03-20 10:52:48 -0500

    chore: updated contentData

--- src/lib/constants/contentData/contentData.constants.ts
+++ src/lib/constants/contentData/contentData.constants.ts
@@ -1,4 +1,8 @@
-export const INTRODUCTION_NAME = "Hello I'am Andrés Moreno.";
-export const INTRODUCTION_JOB_WITH_LOCATION = "Web UI Developer Base in Medellín - Colombia.";
+export const INTRODUCTION_GREET = "Hello I'am";
+export const INTRODUCTION_NAME = "Andrés Moreno.";
+export const INTRODUCTION_JOB = "Web UI";
+export const INTRODUCTION_JOB_TITLE = "Developer";
+export const INTRODUCTION_LOCATION = "Base in";
+export const INTRODUCTION_LOCATION_CITY = "Medellín - Colombia."
 export const INTRODUCTION_DESCRIPTION =
   "Web UI Developer with a passion for continuous learning and embracing new challenges and exploring innovative ideas. Fueled by natural curiosity of achieving exceptional results in every professional undertaking. Demonstrating responsiveness and adaptability to evolve in the workplace demands while keeping a proactive mindset that effectively navigates dynamic environments.";
--- src/lib/constants/contentData/contentData.ts
+++ src/lib/constants/contentData/contentData.ts
@@ -1,35 +1,63 @@
-import { INTRODUCTION_NAME, INTRODUCTION_JOB_WITH_LOCATION, INTRODUCTION_DESCRIPTION } from './contentData.constants';
+import {
+  INTRODUCTION_GREET,
+  INTRODUCTION_NAME,
+  INTRODUCTION_JOB,
+  INTRODUCTION_JOB_TITLE,
+  INTRODUCTION_LOCATION,
+  INTRODUCTION_LOCATION_CITY,
+  INTRODUCTION_DESCRIPTION,
+} from "./contentData.constants";
 
 enum CONTENT {
-	INTRODUCTION_1 = INTRODUCTION_NAME,
-	INTRODUCTION_2 = INTRODUCTION_JOB_WITH_LOCATION,
-	SHORT_DESCRIPTION = INTRODUCTION_DESCRIPTION
-};
-
-const keys = [
-	'introduction_1',
-	'introduction_2',
-	'short_description'
-] as const;
-
-type TValidContentKey = typeof keys[number];
-
-interface IContent {
-	content: string;
-};
-
-type TContent = {
-	[key in TValidContentKey]: IContent
+  GREET = INTRODUCTION_GREET,
+  NAME = INTRODUCTION_NAME,
+  JOB = INTRODUCTION_JOB,
+  JOB_TITLE = INTRODUCTION_JOB_TITLE,
+  LOCATION = INTRODUCTION_LOCATION,
+  LOCATION_CITY = INTRODUCTION_LOCATION_CITY,
+  SHORT_DESCRIPTION = INTRODUCTION_DESCRIPTION,
 }
 
-export const contentData: TContent =  {
-	introduction_1: {
-		content: CONTENT.INTRODUCTION_1
+const keys = [
+  "greet",
+  "name",
+  "job",
+  "job_title",
+  "location",
+  "location_city",
+  "short_description",
+] as const;
+
+type TValidContentKey = (typeof keys)[number];
+
+interface IContent {
+  content: string;
+}
+
+type TContent = {
+  [key in TValidContentKey]: IContent;
+};
+
+export const contentData: TContent = {
+  greet: {
+		content: CONTENT.GREET
 	},
-	introduction_2: {
-		content: CONTENT.INTRODUCTION_2,
+	name: {
+		content: CONTENT.NAME
+	},
+	job: {
+		content: CONTENT.JOB
+	},
+	job_title: {
+		content: CONTENT.JOB_TITLE
+	},
+	location: {
+		content: CONTENT.LOCATION
+	},
+	location_city: {
+		content: CONTENT.LOCATION_CITY
 	},
 	short_description: {
 		content: CONTENT.SHORT_DESCRIPTION
 	}
-}
+};
