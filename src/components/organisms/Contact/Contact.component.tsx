4ed72cae547cc545923dbf546d5961ead881aaff
Andres Moreno <andresmorenojf@gmail.com>
2024-04-07 13:08:33 -0500

    feat: whatsapp link added to the contact info

--- src/components/organisms/Contact/Contact.component.tsx
+++ src/components/organisms/Contact/Contact.component.tsx
@@ -1,4 +1,4 @@
-import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa6";
+import { FaLinkedinIn, FaGithub, FaInstagram, FaWhatsapp } from "react-icons/fa6";
 
 import { IconAnchor } from "@components/molecules";
 import { contactData } from "@constants/index";
@@ -7,15 +7,19 @@ import { StyledContactContainer } from './Contact.styles';
 
 export const Contact: React.FC = () => {
   const contactValues = Object.values(contactData);
-  const icons = [FaLinkedinIn, FaGithub, FaInstagram];
+  const icons = [FaLinkedinIn, FaGithub, FaInstagram, FaWhatsapp];
 
   return (
     <StyledContactContainer>
-      {contactValues.map((item, index) => (
-        <li key={item.label}>
-          <IconAnchor href={item.href} icon={icons[index]} variant="default" />
-        </li>
-      ))}
+      {contactValues.map((item, index) => {
+        console.log(item);
+        
+        return (
+          <li key={item.label}>
+            <IconAnchor href={item.href} icon={icons[index]} variant="default" />
+          </li>
+        )
+      })}
     </StyledContactContainer>
   );
 };
--- src/lib/constants/contactData/contactData.ts
+++ src/lib/constants/contactData/contactData.ts
@@ -1,37 +1,46 @@
+const getWhatsappMessage = (message: string): string => {
+	console.log('message', message);
+	
+  return message.split(" ").reduce((acc, text) => `${acc}%20${text}`, '');
+};
+
+const WHATSAPP_MESSAGE = "Hello Andrés, I just visited your personal web site and I would like to get in touch with you."
+
 enum CONTACT {
-	LINKEDIN = 'https://www.linkedin.com/in/andres-moreno-jf/',
-	GITHUB = 'https://github.com/andresmorenoj',
-	INSTAGRAM = 'https://www.instagram.com/andres.moreno.jf/',
+  LINKEDIN = "https://www.linkedin.com/in/andres-moreno-jf/",
+  GITHUB = "https://github.com/andresmorenoj",
+  INSTAGRAM = "https://www.instagram.com/andres.moreno.jf/",
+  WHATSAPP = "https://wa.me/+573148095997?text=",
 }
 
-const keys = [
-	'linkedin',
-	'github',
-	'instagram',
-] as const;
+const keys = ["linkedin", "github", "instagram", "whatsapp"] as const;
 
-type TValidPageKey = typeof keys[number];
+type TValidPageKey = (typeof keys)[number];
 
 interface IProperties {
-	label: string;
-	href: string;
+  label: string;
+  href: string;
 }
 
 type TContact = {
-	[key in TValidPageKey]: IProperties
-}
+  [key in TValidPageKey]: IProperties;
+};
 
 export const contactData: TContact = {
-	linkedin: {
-		label: 'LinkedIn',
-		href: CONTACT.LINKEDIN
-	}, 
-	github: {
-		label: 'Github',
-		href: CONTACT.GITHUB
-	},
-	instagram: {
-		label: 'Instagram',
-		href: CONTACT.INSTAGRAM
+  linkedin: {
+    label: "LinkedIn",
+    href: CONTACT.LINKEDIN,
+  },
+  github: {
+    label: "Github",
+    href: CONTACT.GITHUB,
+  },
+  instagram: {
+    label: "Instagram",
+    href: CONTACT.INSTAGRAM,
+  },
+	whatsapp: {
+		label: "Whatsapp",
+		href: `${CONTACT.WHATSAPP}${getWhatsappMessage(WHATSAPP_MESSAGE)}`
 	}
 };
