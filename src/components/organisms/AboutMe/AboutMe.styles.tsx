055c672ec4198d2372cc6be575bf1b2f86146b00
Andres Moreno <andresmorenojf@gmail.com>
2024-04-19 23:43:49 -0500

    feat: responsiveness section 5

--- src/components/organisms/AboutMe/AboutMe.styles.tsx
+++ src/components/organisms/AboutMe/AboutMe.styles.tsx
@@ -1,20 +1,40 @@
 import styled, { css } from "styled-components";
 
+import { getMedia } from "@utils/index";
 import { Paragraph } from "@components/atoms";
 import { withMultipleOf } from "@theme/baseTheme/mixins";
 
 export const StyledSectionContainer = styled.section(({ theme }) => {
   return css`
-  padding: 0 ${withMultipleOf("medium", 4)};
+    padding: 0 ${theme.measurements.small};
 
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: 1fr;
     align-items: center;
     gap: ${theme.measurements.extraLarge};
 
     figure {
-      justify-self: end;
-      max-width: 750px;
+      justify-self: center;
+      max-width: 85%;
+
+      @media ${getMedia("mobileFirst", "medium")} {
+        max-width: 70%;
+      }
+
+      @media ${getMedia("mobileFirst", "large")} {
+        justify-self: end;
+        max-width: 750px;
+      }
+    }
+
+    @media ${getMedia("mobileFirst", "medium")} {
+      padding-inline: ${theme.measurements.medium};
+    }
+
+    @media ${getMedia("mobileFirst", "large")} {
+      grid-template-columns: repeat(2, 1fr);
+
+      padding-inline: ${withMultipleOf("medium", 4)};
     }
   `;
 });
